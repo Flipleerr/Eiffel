ae9ef701af28521db0e8c2f7c414624d7ac6337f
memoryleak <glupacsuper2009@gmail.com>
2026-03-13 19:32:42 +0100

    new logger changes (and terminal integration, sort of)

--- Eiffel/Logger.cs
+++ Eiffel/Logger.cs
@@ -1,11 +1,26 @@
 ﻿// introducing, the worst logging system of all time:
 using System;
+using System.Collections.Concurrent;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace Eiffel
 {
     public static class Logger
     {
+        private const uint ATTACH_PARENT_PROCESS = 0xFFFFFFFF;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool AllocConsole();
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool AttachConsole(uint dwProcessId);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool SetStdHandle(int nStdHandle, IntPtr hHandle);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetStdHandle(int nStdHandle);
+
+        public static ConcurrentQueue<string> LogQueue = new ConcurrentQueue<string>();
+
         public enum LogLevel
         {
             Normal,
@@ -21,23 +36,76 @@ namespace Eiffel
         }
 
         public static void Info(string contents)
-        {
-            File.AppendAllText("eiffel.log", $"{DateTime.Now} INFO: " + contents);
-        }
+            => Log(LogLevel.Normal, contents);
 
-        public static void Warn(string contents)
-        {
-            File.AppendAllText("eiffel.log", $"{DateTime.Now} WARNING: " + contents);
-        }
+        public static void Warn(string contents) 
+            => Log(LogLevel.Warn, contents);
 
         public static void Error(string contents)
+            => Log(LogLevel.Error, contents);
+
+        public static void Verbose(string contents) 
+            => Log(LogLevel.Verbose, contents);
+
+        public static void Log(LogLevel level, string contents)
         {
-            File.AppendAllText("eiffel.log", $"{DateTime.Now} ERROR: " + contents);  
+            switch (level)
+            {
+                case LogLevel.Normal:
+                    File.AppendAllText("eiffel.log", $"{DateTime.Now} INFO: " + contents);
+#if DEBUG
+                    Console.WriteLine($"{DateTime.Now} INFO: " + contents + Environment.NewLine);
+#endif
+                    break;
+                case LogLevel.Error:
+                    File.AppendAllText("eiffel.log", $"{DateTime.Now} ERROR: " + contents + Environment.NewLine);
+#if DEBUG
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{DateTime.Now} ERROR: " + contents + Environment.NewLine);
+                    Console.ForegroundColor = ConsoleColor.White;
+#endif
+                    break;
+                case LogLevel.Warn:
+                    File.AppendAllText("eiffel.log", $"{DateTime.Now} WARNING: " + contents + Environment.NewLine);
+#if DEBUG
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"{DateTime.Now} WARNING: " + contents + Environment.NewLine);
+                    Console.ForegroundColor = ConsoleColor.White;
+#endif
+                    break;
+                case LogLevel.Verbose:
+                    File.AppendAllText("eiffel.log", $"{DateTime.Now} VERBOSE: " + contents + Environment.NewLine);
+#if DEBUG
+                    Console.WriteLine($"{DateTime.Now} VERBOSE: " + contents + Environment.NewLine);
+#endif
+                    break;
+
+                default:
+                    File.AppendAllText("eiffel.log", $"{DateTime.Now} INFO: " + contents + Environment.NewLine);
+#if DEBUG
+                    Console.WriteLine($"{DateTime.Now} VERBOSE: " + contents + Environment.NewLine);
+#endif
+                    break;
+            }
         }
 
-        public static void Verbose(string contents)
+        // debug only
+        public static void SetupConsole()
         {
-            File.AppendAllText("eiffel.log", $"{DateTime.Now} VERBOSE: " + contents);
+            if (!AttachConsole(ATTACH_PARENT_PROCESS))
+            {
+                AllocConsole();
+            }
+
+            var stdOut = GetStdHandle(-11);
+
+            SetStdHandle(-11, stdOut);
+
+            var standardOutput = new StreamWriter(Console.OpenStandardOutput());
+            standardOutput.AutoFlush = true;
+            Console.SetOut(standardOutput);
+
+            Logger.Info("Debug console created!");
         }
     }
 }
